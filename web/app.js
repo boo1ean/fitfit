e11d4b84bd236a928798ec530c53c7c51f17c415
boo1ean <boo1ean0807@gmail.com>
2014-03-09 14:39:21 +0200

    Add really awesome handler for * routes

---
+++ web/app.js
@@ -0,0 +1,12 @@
+var express = require('express'),
+    resolve = require('path').resolve;
+
+var port = 3005;
+var app = express();
+
+app.use(express.static(__dirname + '/../static'));
+app.get('*', function(req, res) {
+	res.sendfile(resolve(__dirname + '/../static/index.html'));
+});
+
+app.listen(port);
