31e20522c3bd26bd066745cc89814e3792661232
boo1ean <boo1ean0807@gmail.com>
2014-03-23 21:12:51 +0200

    Adjust some styles

--- static/scripts/app.js
+++ static/scripts/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('fitApp', ['ngRoute', 'ngTouch', 'angularMoment', 'fitApp.services']).
+angular.module('fitApp', ['ngRoute', 'ngTouch', 'angularMoment', 'fitApp.services', 'truncate']).
 	config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
 		$routeProvider.
 			when('/', {
